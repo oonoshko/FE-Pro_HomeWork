789bf37a88e85a58adc5beca9398575c0ec4cab2
Oleg Onoshko <onoshko.oleg@gmail.com>
2022-09-04 10:44:42 +0300

    Add HomeWork2

--- js/script-1.js
+++
@@ -1,5 +0,0 @@
-console.log(5 + 10)
-console.log(5 * 10)
-console.log(10 / 5)
-console.log(25 - 5)
-console.log(25 % 3)
---
+++ script.js
@@ -0,0 +1,27 @@
+let myName = "My name is Oleg";
+let mySurmame = "My surname Onoshko";
+let myСity = "I live in Kyiv";
+
+let averageFourNumbers = (10 + 10 + 5 + 5) / 4;
+let radius = '50';
+const PI_NUMBER = '3.14';
+let areaOfCircle;
+let circleLength;
+
+areaOfCircle = PI_NUMBER * radius ** 2; 
+circleLength = 2 * PI_NUMBER * radius;
+
+// let 1myName = 'Oleg';
+// let #myCity = 'Kyiv';
+// let my-Age = '25';
+
+// Comments number one
+/* Comments number two*/
+
+console.log(myName);
+console.log(mySurmame);
+console.log(myСity);
+console.log(averageFourNumbers);
+console.log(radius);
+console.log(areaOfCircle);
+console.log(circleLength);
