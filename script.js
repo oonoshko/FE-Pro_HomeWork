50f5aa76a5401dce3986ab2e285991c938ed8a9e
Oleg Onoshko <onoshko.oleg@gmail.com>
2022-09-08 18:59:29 +0300

    add several strok

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ let favoriteNumber = prompt('What is your favorite number?');
 console.log(`Hello ${userName} ${userSurname}! Your favorite number is ${favoriteNumber}`);
 
 let num1 = prompt('Enter first number?',0);
-let num2 = prompt('Enter second number?',0);
+let num2 = prompt('Enter second number?', 0);
 
-console.log(`Sum ${num1} + ${num2} is ${+num1 + +num2}`);
-console.log(`Sum ${num1} - ${num2} is ${+num1 - +num2}`);
-console.log(`Sum ${num1} * ${num2} is ${+num1 * +num2}`);
-console.log(`Sum ${num1} / ${num2} is ${+num1 / +num2}`);
+let addition = +num1 + +num2;
+let subtraction = +num1 - +num2;
+let multiplication = +num1 * +num2;
+let division = +num1 / +num2;
+
+console.log(`Sum ${num1} + ${num2} is ${addition}`);
+console.log(`Sum ${num1} - ${num2} is ${subtraction}`);
+console.log(`Sum ${num1} * ${num2} is ${multiplication}`);
+console.log(`Sum ${num1} / ${num2} is ${division}`);
 
 let yearBorn = prompt('In what year were you born?');
 let age = 2022 - yearBorn;
@@ -18,7 +23,8 @@ console.log(`Your age is ${age} years.`);
 
 let number1 = 125;
 let number2 = 10;
-console.log(`Остача від ділення числа ${number1} на число ${number2} рівна ${number1 %= number2}`);
+let resultModulus = `Остача від ділення числа ${number1} на число ${number2} рівна ${number1 %= number2}`;
+console.log(resultModulus);
 
 let number = prompt('Enter a number?',0);
 result = (number % 2 == 0) ? "even" : "odd";
