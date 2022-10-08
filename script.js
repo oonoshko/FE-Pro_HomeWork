d9b9cc4c6aaea58096f46a0b6d20aff07bb5f2f1
Oleg Onoshko <onoshko.oleg@gmail.com>
2022-10-08 21:09:24 +0300

    fixed mistakes after feedback

--- script.js
+++ script.js
@@ -12,23 +12,21 @@ function Person(name, age, sex, nationality, country, travelList) {
   this.travelList = travelList;
 }
 
-const oleg = {
-  name: "Oleg",
-  age: 38,
-  sex: "man",
-  nationality: "ukrainian",
-  country: "Ukraine",
-  travelList: ["USA", "Canada", "Spain", "Portunal", "Italy"],
-};
+const oleg = new Person("Oleg", 38, "man", "Ukrainian", "Ukraine", [
+  "USA",
+  "Canada",
+  "Spain",
+  "Portugal",
+  "Italy",
+]);
 
-const iryna = {
-  name: "Iryna",
-  age: 33,
-  sex: "women",
-  nationality: "ukrainian",
-  country: "Ukraine",
-  travelList: ["Japan", "Malta", "Spain", "Singapur", "Italy"],
-};
+const iryna = new Person("Iryna", 33, "women", "Ukrainian", "Ukraine", [
+  "Japan",
+  "Malta",
+  "Spain",
+  "Singapur",
+  "Italy",
+]);
 
 function acquaintance() {
   console.group(`${this.name} info:`);
@@ -52,72 +50,62 @@ function sleep() {
     `${this.name} which has ${this.age} years should go to sleep at 22:30. He/She did this in ${this.country}.`
   );
 }
-function country() {
+function getCountry() {
   console.log(
     `${this.name} is a ${this.sex} would like to travel to next countries ${this.travelList}.`
   );
 }
 
-const person = new Person();
-
 acquaintance.apply(oleg);
 wakeUp.apply(oleg);
 sleep.apply(oleg);
-country.apply(oleg);
+getCountry.apply(oleg);
 
 acquaintance.call(iryna);
 wakeUp.call(iryna);
 sleep.call(iryna);
-country.call(iryna);
+getCountry.call(iryna);
 
 /* Створити власну реалізацію методу .bind (підказка: в кінці треба помістити цю власну функцію у прототип -
   Function.prototype.myOwnBind = function () { [тут_код_кастомного_bind] } */
 
-let myFunc = function (city) {
+const myFunc = function (city) {
   console.log(`My name is ${this.name} and I like my city ${city}.`);
 };
 
 Function.prototype.myOwnBind = function (oleg, ...args) {
-  let func = this;
+  const func = this;
   return function () {
     func.apply(oleg, [...args]);
   };
 };
 
-let newFunc = myFunc.myOwnBind(oleg, `Kyiv`);
+const newFunc = myFunc.myOwnBind(oleg, "Kyiv");
 newFunc();
 
 /* Cтворити функцію, котра приймає 2 параметри - об'єкти. Функція повинна перевіряти чи ці 2 обʼєкти абсолютно ідентичні та повертає результат у зрозумілому форматі */
 
-firstObj = { name: "Oleg", age: 38 };
-secondObj = { name: "Oleg", age: 38 };
+const firstObj = { name: "Oleg", age: 38, one: { two: 2 } };
+const secondObj = { name: "Oleg", age: 38, one: { two: 2 } };
 
-function objectIqualsOrNot(firstObj, secondObj) {
+function isObjectEquals(firstObj, secondObj) {
   if (
     firstObj === null ||
     secondObj === null ||
     firstObj === undefined ||
-    secondObj === undefined ||
-    firstObj === "" ||
-    secondObj === "" ||
-    firstObj === 0 ||
-    secondObj === 0
+    secondObj === undefined
   ) {
     console.log(
-      `You entered a non-existing object. Please enter real objects for comparison.`
+      "You entered a non-existing object. Please enter real objects for comparison."
     );
   } else {
-    this.firstObj = firstObj;
-    this.secondObj = secondObj;
-    const result = Object.keys({ ...firstObj, ...secondObj }).every(
-      (key) => firstObj[key] === secondObj[key]
-    );
-    console.log(`These two objects iquals each other or not? It's ${result}.`);
+    const result = JSON.stringify(firstObj) === JSON.stringify(secondObj);
+    console.log(`These two objects equals each other or not? It's ${result}.`);
   }
 }
-objectIqualsOrNot();
+isObjectEquals();
 
-// objectIqualsOrNot(firstObj, secondObj);
+isObjectEquals(firstObj, secondObj);
 
 /* Створіть функцію-конструктор Calculator, який створює об’єкти з трьома методами:
 enterData - запитує два значення за допомогою prompt і запам’ятовує їх у властивостях об’єкта.
@@ -125,51 +113,52 @@ calculateSum() повертає суму цих властивостей.
 calculateNSD() повертає результат пошуку НСД
 calculateNSK() повертає результат пошуку НСК */
 
-let firstNumber = +prompt(`Please enter first any number.`);
-let secondNumber = +prompt("Please enter second any number.");
-const enterData = { firstNumber, secondNumber };
-
-if (firstNumber <= 0 || firstNumber === "" || isNaN(firstNumber)) {
-  console.log(
-    `You entered incorrect first number. Please enter number more than 0.`
-  );
-} else if (secondNumber <= 0 || secondNumber === "" || isNaN(secondNumber)) {
-  console.log(
-    `You entered incorrect second number. Please enter number more than 0`
-  );
-} else {
-  function Calculator(enterData) {
+function Calculator(firstNumber, secondNumber) {
+  this.enterData = function () {
     this.firstNumber = firstNumber;
     this.secondNumber = secondNumber;
+    console.log(firstNumber, secondNumber);
+    if (firstNumber <= 0 || firstNumber === "" || isNaN(firstNumber)) {
+      console.log(
+        "You entered incorrect first number. Please enter number more than 0."
+      );
+    } else if (
+      secondNumber <= 0 ||
+      secondNumber === "" ||
+      isNaN(secondNumber)
+    ) {
+      console.log(
+        "You entered incorrect second number. Please enter number more than 0"
+      );
+    } else {
+      this.calculateSum = function () {
+        return this.firstNumber + this.secondNumber;
+      };
 
-    this.calculateSum = function () {
-      return this.firstNumber + this.secondNumber;
-    };
-
-    this.calculateNSD = function () {
-      while (this.firstNumber != this.secondNumber) {
-        if (this.firstNumber > this.secondNumber) {
-          this.firstNumber = this.firstNumber - this.secondNumber;
-        } else {
-          this.secondNumber = this.secondNumber - this.firstNumber;
+      this.calculateNSD = function () {
+        while (this.firstNumber !== this.secondNumber) {
+          if (this.firstNumber > this.secondNumber) {
+            this.firstNumber -= this.secondNumber;
+          } else {
+            this.secondNumber -= this.firstNumber;
+          }
         }
-      }
-      return this.firstNumber;
-    };
+        return this.firstNumber;
+      };
 
-    this.calculateNSK = function () {
-      return (firstNumber * secondNumber) / this.calculateNSD();
-    };
-  }
+      this.calculateNSK = function () {
+        return (firstNumber * secondNumber) / this.calculateNSD();
+      };
+    }
+  };
 }
 
-let calculator = new Calculator();
+const calculator = new Calculator(
+  +prompt("Please enter first any number."),
+  +prompt("Please enter second any number.")
+);
+calculator.enterData();
 
-calculator.calculateSum();
-calculator.calculateNSK();
-calculator.calculateNSD();
-// console.log(enterData.firstNumber, enterData.secondNumber);
-
-// console.log("Sum=" + calculator.calculateSum());
-// console.log("NSD=" + calculator.calculateNSD());
-// console.log("NSK=" + calculator.calculateNSK());
+console.log(`Sum=${calculator.calculateSum()}`);
+console.log(`NSD=${calculator.calculateNSD()}`);
+console.log(`NSK=${calculator.calculateNSK()}`);
