3680d074585943862ffe27ac47ac8b1787b12145
Oleg Onoshko <onoshko.oleg@gmail.com>
2022-11-23 10:51:42 +0200

    ready HomeWork16

--- script.js
+++ script.js
@@ -3,21 +3,19 @@ const API_KEY = "27ff6d2e";
 const input = document.querySelector("#input1");
 const button = document.querySelector("#btn");
 const result = document.querySelector("#result");
-
+const pagination = document.querySelector("#pagination");
 const detailsMovie = document.querySelector(".detailsMovie");
 
 let counter = 1;
 const MoviesPerPage = 10;
 
 const loadMovies = function () {
-  const url = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${input.value}&page=${counter}`;
+  const url = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${input.value}&page=${counter}`; // тут я захордкодив значення batman й пагінація працює
 
-  const getPromise = fetch(url);
-
-  getPromise
-    .then((success) => {
-      if (success.status === 200) {
-        return success.json();
+  fetch(url)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
       }
     })
     .then((movies) => {
@@ -29,31 +27,45 @@ const loadMovies = function () {
       displayPagination(moviesTotalList);
     })
     .catch((error) => {
+      input.value = "";
+      pagination.innerHTML = "";
       error = document.createElement("div");
       error.classList.add("mistake");
-      error.innerHTML = `
-      <img class='error' src='./images/notfound.png' alt="pictupe">
-      <p class='textError'>Movie not found</p>`;
+
+      const errorElImg = document.createElement("img");
+      errorElImg.classList.add("error");
+      errorElImg.src = "./images/notfound.png";
+
+      const errorElP = document.createElement("p");
+      errorElP.classList.add("textError");
+      errorElP.innerHTML = "Movie not found";
+
+      error.append(errorElImg, errorElP);
       result.append(error);
-      console.log("Movie not found");
     });
-  input.value = "";
-  // counter += 1;
 };
 
 const displayPagination = function (total) {
-  const pagination = document.querySelector("#pagination");
-  for (let i = 0; i < total / MoviesPerPage; i++) {
+  pagination.innerHTML = "";
+  detailsMovie.innerHTML = "";
+
+  for (let i = 0; i < total / MoviesPerPage && i < 100; i++) {
     const spanEl = document.createElement("span");
+    spanEl.classList.add("page");
 
     spanEl.innerHTML = i + 1;
 
     pagination.append(spanEl);
 
     spanEl.addEventListener("click", (event) => {
-      counter = event.target;
-      console.log(counter);
+      event.preventDefault();
+      counter = +event.target.innerText;
+      loadMovies();
+      detailsMovie.innerHTML = "";
     });
+    if (counter !== 1) {
+      counter = 1;
+    }
   }
 };
 
@@ -63,23 +75,27 @@ const displayMovies = function (moviesList) {
   moviesList.forEach((elem) => {
     const cardMovies = document.createElement("div");
     cardMovies.classList.add("cardMovies");
+
+    const imgEl = document.createElement("img");
+    imgEl.classList.add("poster");
+
+    elem.Poster !== "N/A"
+      ? (imgEl.src = elem.Poster)
+      : (imgEl.src = "./images/image_not_found.png");
+
+    const pTitle = document.createElement("p");
+    pTitle.classList.add("title");
+    pTitle.innerHTML = elem.Title;
+
+    const pYear = document.createElement("p");
+    pYear.classList.add("year");
+    pYear.innerHTML = elem.Year;
+
     const bntDetails = document.createElement("button");
     bntDetails.classList.add("moreDetails");
-
-    if (elem.Poster !== "N/A") {
-      cardMovies.innerHTML = `
-        <img class='poster' src='${elem.Poster}' alt="${elem.Title}">
-        <p class='title'>${elem.Title}</p>
-        <p class='year'>${elem.Year}</p>`;
-    } else {
-      cardMovies.innerHTML = `
-        <img class='poster' src='./images/image_not_found.png' alt="pictupe">
-        <p class='title'>${elem.Title}</p>
-        <p class='year'>${elem.Year}</p>`;
-    }
-
     bntDetails.innerHTML = "Details";
-    cardMovies.appendChild(bntDetails);
+
+    cardMovies.append(imgEl, pTitle, pYear, bntDetails);
 
     bntDetails.addEventListener("click", () => {
       loadDetails(elem.imdbID);
@@ -93,9 +109,7 @@ button.addEventListener("click", loadMovies);
 const loadDetails = function (id) {
   const urlDetails = `http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`;
 
-  const getDataDetails = fetch(urlDetails);
-
-  getDataDetails
+  fetch(urlDetails)
     .then((success) => {
       if (success.status === 200) {
         return success.json();
@@ -109,22 +123,43 @@ const loadDetails = function (id) {
     });
 };
 
-const cardMoviesDetails = document.createElement("div");
-cardMoviesDetails.classList.add("cardMoviesDetails");
-
 const displayMoviesDetails = function (info) {
-  console.log(info);
+  const cardMoviesDetails = document.createElement("div");
+  cardMoviesDetails.classList.add("cardMoviesDetails");
+
   detailsMovie.innerHTML = "";
 
-  cardMoviesDetails.innerHTML = `
-    <p>Name: ${info.Title}</p>
-    <p>${info.Year} release year</p>
-    <p>Genre: ${info.Genre}</p>
-    <p>Actors: ${info.Actors}</p>
-    <p>Discription: ${info.Plot}</p>
-    <p>Runtime: ${info.Runtime}</p>        
-    <p>Rating: ${info.imdbRating}</p>        
-    `;
+  const name = document.createElement("p");
+  name.innerHTML = info.Title;
 
-  detailsMovie.appendChild(cardMoviesDetails);
+  const genre = document.createElement("p");
+  genre.innerHTML = `Genre: ${info.Genre}`;
+
+  const discription = document.createElement("p");
+  info.Plot !== "N/A"
+    ? (discription.innerHTML = `Discription: ${info.Plot}`)
+    : (discription.innerHTML = "Discription: Not found.");
+
+  const runtime = document.createElement("p");
+  info.Runtime !== "N/A"
+    ? (runtime.innerHTML = `Runtime: ${info.Runtime}`)
+    : (runtime.innerHTML = "Runtime: Not found.");
+
+  const actors = document.createElement("p");
+  info.Actors !== "N/A"
+    ? (actors.innerHTML = `Actors: ${info.Actors}`)
+    : (actors.innerHTML = "Actors: Not found.");
+
+  const rating = document.createElement("p");
+  info.imdbRating !== "N/A"
+    ? (rating.innerHTML = `Rating: ${info.imdbRating}`)
+    : (rating.innerHTML = "Rating: Not found.");
+
+  cardMoviesDetails.append(name, genre, discription, runtime, actors, rating);
+  detailsMovie.append(cardMoviesDetails);
 };
+
+window.addEventListener("click", (e) => {
+  e.preventDefault();
+  detailsMovie.innerHTML = "";
+});
